add9230765457c9f8bb4a30285ce0c173c6f0ab1
Rony Lutsky <ronlut@gmail.com>
2020-04-23 20:40:16 +0300

    separate minikube build script from standard

--- minikube-build-image.sh
+++ minikube-build-image.sh
@@ -1,5 +1,2 @@
 eval $(minikube docker-env)
-base_dir=$(pwd)
-mvn package
-cd build-image
-./build.sh --from-release --flink-version 1.9.2 --scala-version 2.12 --job-artifacts "$base_dir/target/flinkjobcluster-1.0-SNAPSHOT.jar" --image-name flink-k8s-example
+./build-image.sh
